51f7c530368ab658c59789fc26adc81eeb4f3af3
Robert Myers <romyers@umich.edu>
2023-12-14 17:09:27 -0500

    See changelog

---
+++ ROOT_plot/CMakeFiles/test,.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/muonuser/gitfolder/phase2_MiniDAQ_Robert/ROOT_plot/macros/test.cpp" "/home/muonuser/gitfolder/phase2_MiniDAQ_Robert/ROOT_plot/CMakeFiles/test,.dir/macros/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/ROOT/include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ROOT_plot/macros/test.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+
+#include "TH1.h"
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World" << endl;
+
+}
